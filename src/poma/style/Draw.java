8cb58d8fa3ae59ad8f53244312ba037e1786eea5
gonzalo <gonzalocrp@outlook.com>
2018-09-09 00:37:04 +0200

    New class inside style package

---
+++ src/poma/style/Draw.java
@@ -0,0 +1,13 @@
+package poma.style;
+
+import java.io.Serializable;
+
+public class Draw implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public String draw() {
+		return "";
+	}
+
+}
